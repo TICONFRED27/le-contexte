d8015d5a86f4c775dadcda55afa2b37a00157ccd
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-06 10:35:02 +0100

    Update App.jsx

--- Contexte/src/App.jsx
+++ Contexte/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import MoviesList from "./components/MoviesList";
+import MovieContext from "./contexts/MovieContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = React.useState([
+    {
+      movie_id: 1,
+      title: "Harry Potter and the Sorcerers Stone",
+      release_year: 2001,
+    },
+    {
+      movie_id: 2,
+      title: "Harry Potter and the Chamber of Secrets",
+      release_year: 2002,
+    },
+    {
+      movie_id: 3,
+      title: "Harry Potter and the Prison of Azkaban",
+      release_year: 2004,
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MovieContext.Provider value={{ movies }}>
+      <MoviesList />
+    </MovieContext.Provider>
+  );
 }
 
-export default App
+export default App;
